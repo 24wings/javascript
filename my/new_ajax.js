e04d547f009a5c7dd51cc5206c62c8fd796cee19
webopen <1066306126@qq.com>
2017-03-23 15:00:34 +0800

    first commit

---
+++ my/new_ajax.js
@@ -0,0 +1,40 @@
+function ajax(url, fnSucc, fnFaild)
+{
+	//1.创建ajax对象
+	var oAjax=null;
+	
+	if(window.XMLHttpRequest)
+	{
+		oAjax=new XMLHttpRequest();
+	}
+	else
+	{
+		oAjax=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	
+	//2.连接服务器
+	oAjax.open('GET', url, true);
+	
+	//3.发送请求
+	oAjax.send();
+	
+	//4.接收返回
+	oAjax.onreadystatechange=function ()
+	{
+		if(oAjax.readyState==4)
+		{
+			if(oAjax.status==200)
+			{
+				//alert('成功：'+oAjax.responseText);
+				fnSucc(oAjax.responseText);
+			}
+			else
+			{
+				if(fnFaild)
+				{
+					fnFaild();
+				}
+			}
+		}
+	};
+}
